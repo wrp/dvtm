2e4fd2a48b591068f959b715b6eb01894d25880b
William Pursell <william.r.pursell@gmail.com>
2020-04-17 19:34:58 +0000

    Use color pair 9 for RED
    This is a mess.  Probably should just abandon support for 256 colors

--- dvtm.c
+++ dvtm.c
@@ -82,9 +82,9 @@ enum {
 };
 
 static struct color colors[] = {
-	[DEFAULT] = { .fg = -1,         .bg = -1, .fg256 = -1, .bg256 = -1, },
-	[BLUE]    = { .fg = COLOR_BLUE, .bg = -1, .fg256 = 68, .bg256 = -1, },
-	[RED]     = { .fg = COLOR_RED,  .bg = -1, .fg256 = 0, .bg256 = 0, },
+	[DEFAULT] = { .fg = -1,         .bg = -1, .fg256 =  -1, .bg256 = -1, },
+	[BLUE]    = { .fg = COLOR_BLUE, .bg = -1, .fg256 =  68, .bg256 = -1, },
+	[RED]     = { .fg = COLOR_RED,  .bg = -1, .fg256 =   9, .bg256 = -1, },
 };
 
 const int tags = 8;
