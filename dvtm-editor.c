f1d2760db11c7c1ced646be4e778d063d3147209
William Pursell <william.r.pursell@gmail.com>
2020-04-18 19:35:07 +0000

    Cleanup dvtm-editor
    Handle TMPDIR, check return values of reads, and aesthetic changes

--- dvtm-editor.c
+++ dvtm-editor.c
@@ -18,6 +18,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <limits.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdarg.h>
@@ -26,155 +27,177 @@
 #include <string.h>
 #include <unistd.h>
 
-static void error(const char *msg, ...) {
+int tmp_fd = -1;
+char tempname[PATH_MAX];
+void (*stop)(int) = exit;
+
+void
+cleanup(void)
+{
+	if( tmp_fd != -1 && unlink(tempname) == -1) {
+		perror(tempname);
+	}
+}
+
+static void
+error(int errnum, const char *msg, ...)
+{
 	va_list ap;
 	va_start(ap, msg);
 	vfprintf(stderr, msg, ap);
 	va_end(ap);
-	if (errno)
-		fprintf(stderr, ": %s", strerror(errno));
-	fprintf(stderr, "\n");
+	if( errnum ) {
+		fprintf(stderr, ": %s", strerror(errnum));
+	}
+	fputc('\n', stderr);
+	cleanup();
+	stop(EXIT_FAILURE);
 }
 
-int main(int argc, char *argv[])
+const char *
+get_default_editor(void)
 {
-	int exit_status = EXIT_FAILURE, tmp_write = -1;
-
 	const char *editor = getenv("DVTM_EDITOR");
-	if (!editor)
+	if( editor == NULL ) {
 		editor = getenv("VISUAL");
-	if (!editor)
+	}
+	if( editor == NULL ) {
 		editor = getenv("EDITOR");
-	if (!editor)
+	}
+	if( editor == NULL ) {
 		editor = "vi";
-
-	char tempname[] = "/tmp/dvtm-editor.XXXXXX";
-	if ((tmp_write = mkstemp(tempname)) == -1) {
-		error("failed to open temporary file `%s'", tempname);
-		goto err;
 	}
+	return editor;
+}
 
-	/* POSIX does not mandates modes of temporary file. */
-	if (fchmod(tmp_write, 0600) == -1) {
-		error("failed to change mode of temporary file `%s'", tempname);
-		goto err;
+static void
+build_template(void)
+{
+	char *template = "mvtm-editor.XXXXXX";
+	char *tmpdir = getenv("TMPDIR");
+	int length = 0;
+
+	if( tmpdir ) {
+		size_t cap = sizeof tempname - sizeof template - 2;
+		length = snprintf(tempname, cap, "%s", tmpdir);
+		if( tempname[length - 1] != '/' ) {
+			tempname[length++] = '/';
+		}
+		if( length >= cap ) {
+			error(0, "TMPDIR too long!  Unable to build tmpfile name.");
+		}
 	}
+	tempname[length] = '\0';
+	strncat(tempname, template, sizeof tempname - length - 1);
+}
 
-	char buffer[2048];
+
+int
+main(int argc, char *argv[])
+{
+	char buffer[BUFSIZ];
 	ssize_t bytes;
-	while ((bytes = read(STDIN_FILENO, buffer, sizeof(buffer))) > 0) {
+	struct stat stat_before;
+	const char *editor = get_default_editor();
+
+	build_template();
+	if( (tmp_fd = mkstemp(tempname)) == -1 ) {
+		error(errno, "failed mkstemp %s", tempname);
+	}
+	if( fchmod(tmp_fd, 0600) == -1 ) {
+		error(errno, "failed chmod %s", tempname);
+	}
+	while( (bytes = read(STDIN_FILENO, buffer, sizeof buffer )) > 0 ) {
+		if( bytes < 0 ) {
+			error(errno, "failed to read from stdin");
+		}
 		do {
-			ssize_t written = write(tmp_write, buffer, bytes);
-			if (written == -1) {
-				error("failed to write data to temporary file `%s'",
-				      tempname);
-				goto err;
+			ssize_t written = write(tmp_fd, buffer, bytes);
+			if( written == -1 ) {
+				error(errno, "failed to write to %s", tempname);
 			}
 			bytes -= written;
 		} while (bytes > 0);
 	}
-
-	if (fsync(tmp_write) == -1) {
-		error("failed to fsync temporary file `%s'", tempname);
-		goto err;
+	if( fsync(tmp_fd) == -1 ) {
+		error(errno, "failed to fsync %s", tempname);
 	}
-
-	struct stat stat_before;
-	if (fstat(tmp_write, &stat_before) == -1) {
-		error("failed to stat newly created temporary file `%s'", tempname);
-		goto err;
+	if( fstat(tmp_fd, &stat_before) == -1 ) {
+		error(errno, "failed to stat %s", tempname);
 	}
-
-	if (close(tmp_write) == -1) {
-		error("failed to close temporary file `%s'", tempname);
-		goto err;
+	if( close(tmp_fd) == -1 ) {
+		error(errno, "failed to close %s", tempname);
 	}
-
 	pid_t pid = fork();
-	if (pid == -1) {
-		error("failed to fork editor process");
-		goto err;
-	} else if (pid == 0) {
+	if( pid == -1 ) {
+		error(errno, "failed to fork editor process");
+	} else if( pid == 0 ) {
+		stop = _exit;
+		tmp_fd = -1;
 		int tty = open("/dev/tty", O_RDWR);
-		if (tty == -1) {
-			error("failed to open /dev/tty");
-			_exit(1);
+		if( tty == -1 ) {
+			error(errno, "failed to open /dev/tty");
 		}
-
-		if (dup2(tty, STDIN_FILENO) == -1) {
-			error("failed to set tty as stdin");
-			_exit(1);
+		if( dup2(tty, STDIN_FILENO) == -1 ) {
+			error(errno, "failed to set tty as stdin");
 		}
-
-		if (dup2(tty, STDOUT_FILENO) == -1) {
-			error("failed to set tty as stdout");
-			_exit(1);
+		if( dup2(tty, STDOUT_FILENO) == -1 ) {
+			error(errno, "failed to set tty as stdout");
 		}
-
-		if (dup2(tty, STDERR_FILENO) == -1) {
-			error("failed to set tty as stderr");
-			_exit(1);
+		if( dup2(tty, STDERR_FILENO) == -1 ) {
+			error(errno, "failed to set tty as stderr");
 		}
-
-		close(tty);
-
-		const char *editor_argv[argc+2];
+		if( close(tty) == -1 ) {
+			error(errno, "failed to close tty");
+		}
+		const char *editor_argv[argc + 2];
 		editor_argv[0] = editor;
-		for (int i = 1; i < argc; i++)
+		for( int i = 1; i < argc; i++ ) {
 			editor_argv[i] = argv[i];
+		}
 		editor_argv[argc] = tempname;
-		editor_argv[argc+1] = NULL;
+		editor_argv[argc + 1] = NULL;
 
 		execvp(editor, (char* const*)editor_argv);
-		error("failed to exec editor process `%s'", editor);
-		_exit(127);
+		error(errno, "failed to exec %s", editor);
+		_exit(EXIT_FAILURE);
 	}
-
 	int status;
-	if (waitpid(pid, &status, 0) == -1) {
-		error("waitpid failed");
-		goto err;
+	if( waitpid(pid, &status, 0 ) == -1 ) {
+		error(errno, "waitpid failed");
 	}
-	if (!WIFEXITED(status)) {
-		error("editor invocation failed");
-		goto err;
+	if( WIFSIGNALED(status) ) {
+		error(0, "editor %s (pid %ld) terminated by signal %d",
+			editor, (long)pid, WTERMSIG(status));
 	}
-	if ((status = WEXITSTATUS(status)) != 0) {
-		error("editor terminated with exit status: %d", status);
-		goto err;
+	if( !WIFEXITED(status) ) {
+		error(0, "unexpedted editor invocation failure");
 	}
-
+	if( (status = WEXITSTATUS(status)) != 0 ) {
+		error(0, "editor terminated with exit status: %d", status);
+	}
 	int tmp_read = open(tempname, O_RDONLY);
-	if (tmp_read == -1) {
-		error("failed to open for reading of edited temporary file `%s'",
-		      tempname);
-		goto err;
+	if( tmp_read == -1 ) {
+		error(errno, "failed to open for reading %s", tempname);
 	}
-
 	struct stat stat_after;
-	if (fstat(tmp_read, &stat_after) == -1) {
-		error("failed to stat edited temporary file `%s'", tempname);
-		goto err;
+	if( fstat(tmp_read, &stat_after ) == -1) {
+		error(errno, "failed to stat edited temporary file %s", tempname);
 	}
-
-	if (stat_before.st_mtime == stat_after.st_mtime)
-		goto ok; /* no modifications */
-
-	while ((bytes = read(tmp_read, buffer, sizeof(buffer))) > 0) {
-		do {
-			ssize_t written = write(STDOUT_FILENO, buffer, bytes);
-			if (written == -1) {
-				error("failed to write data to stdout");
-				goto err;
+	if( stat_before.st_mtime != stat_after.st_mtime ) {
+		while( (bytes = read(tmp_read, buffer, sizeof(buffer))) > 0 ) {
+			if( bytes < 0 ) {
+				error(errno, "failed reading %s", tempname);
 			}
-			bytes -= written;
-		} while (bytes > 0);
+			do {
+				ssize_t written = write(STDOUT_FILENO, buffer, bytes);
+				if( written == -1 ) {
+					error(errno, "failed to write data to stdout");
+				}
+				bytes -= written;
+			} while( bytes > 0 );
+		}
 	}
-
-ok:
-	exit_status = EXIT_SUCCESS;
-err:
-	if (tmp_write != -1)
-		unlink(tempname);
-	return exit_status;
+	cleanup();
+	return EXIT_SUCCESS;
 }
