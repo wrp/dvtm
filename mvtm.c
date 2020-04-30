a461b4aa31dc0a76105a0b5d0d68d6fa119d4dbe
William Pursell <williamp@wepay.com>
2020-04-30 19:10:39 +0000

    Remove sel from select_client()

--- mvtm.c
+++ mvtm.c
@@ -1299,7 +1299,7 @@ end:
 static struct client *
 select_client(const struct view *v)
 {
-	struct client *c = sel;
+	struct client *c;
 
 	c = state.current_view->vfocus->c;
 	if( state.buf.count != 0 ) {
